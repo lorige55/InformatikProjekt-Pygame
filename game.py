6b8ac9bc92e58bcd9a40de2a92d1485404a801ad
lorige55 <loris.laetsch@gmail.com>
2025-10-29 13:16:11 +0100

    Added game assets; created basic pygame setup

--- game.py
+++ game.py
@@ -1,1 +1,21 @@
-print("Hello World")
+import pygame
+
+# pygame setup gemäss quickstart vorlage - pygame.org
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(24)  # limits FPS to 24
+
+pygame.quit()
