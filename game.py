33ab98ea481fb6f793f044d6a2baa91357e32ddf
lorige55 <loris.laetsch@gmail.com>
2025-10-30 17:55:37 +0100

    Added linter in local environment --> formatted code

--- game.py
+++ game.py
@@ -1,31 +1,38 @@
-import pygame
+"""The main file of our game"""
+
+import pygame as pg
 
 # pygame setup gemäss quickstart vorlage - pygame.org
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-width = 16
-height = 9
-clock = pygame.time.Clock()
-running = True
+pg.init()
+screen: pg.Surface = pg.display.set_mode((1280, 720))
+WIDTH: int = 16
+HEIGHT: int = 9
+clock: pg.time.Clock = pg.time.Clock()
+running: bool = True
 
 # background
-grassTileOG = pygame.image.load("./assets/tiles/towerDefense_tile231.png").convert()    # load the image
-grassTile = pygame.transform.scale(grassTileOG, (1280/width, 720/height))   # scale the image
+grass_tile_og = pg.image.load(
+    "./assets/tiles/towerDefense_tile231.png"
+).convert()  # load the image
+grass_tile = pg.transform.scale(
+    grass_tile_og, (1280 / WIDTH, 720 / HEIGHT)
+)  # scale the image
+
 # fill the screen with the grass tile
-for i in range(width):
-    for j in range(height):
-        screen.blit(grassTile, (i * (1280/width), j * (720/height)))
+for i in range(WIDTH):
+    for j in range(HEIGHT):
+        screen.blit(grass_tile, (i * (1280 / WIDTH), j * (720 / HEIGHT)))
 
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in pg.event.get():
+        if event.type == pg.constants.QUIT:
             running = False
 
     # RENDER YOUR GAME HERE
 
     # flip() the display to put your work on screen
-    pygame.display.flip()
+    pg.display.flip()
 
     clock.tick(24)  # limits FPS to 24
 
-pygame.quit()
+pg.quit()
