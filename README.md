56eb9621bd61aaa71e61b10ea6fc24131b5874ed
lorige55 <loris.laetsch@gmail.com>
2025-10-30 11:43:47 +0100

    Added tile proof of concept

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Informatik Projekt - Gabriel, Philipe, Loris
+## Credits:
+Game Assets from kenney.nl
--- game.py
+++ game.py
@@ -3,8 +3,15 @@ import pygame
 # pygame setup gemäss quickstart vorlage - pygame.org
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
+width = 16
+height = 9
 clock = pygame.time.Clock()
 running = True
+
+# background
+grassTileOG = pygame.image.load("./assets/tiles/towerDefense_tile231.png").convert()
+grassTile = pygame.transform.scale(grassTileOG, (1280/width, 720/height))
+screen.blit(grassTile, (0, 0))
 
 while running:
     for event in pygame.event.get():
