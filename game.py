4a95e00aa27a1bc3b025bdc8e370b5020b6cc3f3
lorige55 <loris.laetsch@gmail.com>
2025-10-30 12:12:39 +0100

    Added base background to screen

--- game.py
+++ game.py
@@ -9,9 +9,12 @@ clock = pygame.time.Clock()
 running = True
 
 # background
-grassTileOG = pygame.image.load("./assets/tiles/towerDefense_tile231.png").convert()
-grassTile = pygame.transform.scale(grassTileOG, (1280/width, 720/height))
-screen.blit(grassTile, (0, 0))
+grassTileOG = pygame.image.load("./assets/tiles/towerDefense_tile231.png").convert()    # load the image
+grassTile = pygame.transform.scale(grassTileOG, (1280/width, 720/height))   # scale the image
+# fill the screen with the grass tile
+for i in range(width):
+    for j in range(height):
+        screen.blit(grassTile, (i * (1280/width), j * (720/height)))
 
 while running:
     for event in pygame.event.get():
